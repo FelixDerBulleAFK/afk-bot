8e44ba3b7af07e285f2efecc5c63852e249dd4a6
FelixDerBulleAFK <felixg50001@gmail.com>
2026-03-05 13:20:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,31 @@
 const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
-  host: 'Windsmp.de', // Hier die IP deines Servers eintragen
-  port: 19132,             // Meistens 19132 bei Bedrock
-  username: 'MeinAFKBot',
-  version: false           // 'false' lässt den Bot die Version automatisch raten
+  host: 'Windsmp.de', 
+  port: 19132,             
+  username: 'FelixDerBulleAFK',
+  version: false           // Sucht sich automatisch die richtige Version
 })
 
+// Wenn der Bot den Server betritt
 bot.on('spawn', () => {
-  console.log('Erfolg! Der Bot ist auf dem Server.')
+  console.log('FelixDerBulleAFK ist jetzt auf Windsmp.de online!')
+  
+  // Kurze Pause zum Laden, dann zum Zielort (falls nötig)
+  setTimeout(() => {
+    // Falls du dich teleportieren musst, entferne die zwei Schrägstriche vorne:
+    // bot.chat('/home farm') 
+  }, 3000)
 })
 
-bot.on('error', (err) => {
-  console.log('Fehler aufgetreten: ' + err)
-})
+// Anti-AFK: Alle 30 Sekunden einmal zuschlagen
+setInterval(() => {
+  if (bot.entity) {
+    bot.swingArm('right') // Schlägt in die Luft (oder auf ein Monster vor ihm)
+    console.log('Schlag ausgeführt, um AFK-Kick zu verhindern.')
+  }
+}, 30000)
+
+// Fehler abfangen, damit das Skript nicht abstürzt
+bot.on('error', (err) => console.log('Fehler:', err))
+bot.on('kicked', (reason) => console.log('Vom Server gekickt:', reason))
